b58febd0b5ca443220c793a1f30759b386fcf9f7
Lee EunGyeong <eunclid0202@gmail.com>
2024-02-23 23:50:04 +0900

    fix: 지도 다운로드 완료 여부 브로드캐스트 리시버 등록 해제 로직 변경

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/map/MapDownloader.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/map/MapDownloader.kt
@@ -3,6 +3,7 @@ package com.saveurlife.goodnews.map
 import android.app.DownloadManager
 import android.content.*
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
 import android.util.Log
 import android.widget.Toast
@@ -18,7 +19,7 @@ class MapDownloader(private val context: Context) {
     private var downloadId: Long = 0
 
     fun downloadFile(url: String, outputFilePath: String) {
-        Log.d("mapDownloader","파일 다운로드 함수 호출")
+        Log.d("mapDownloader", "파일 다운로드 함수 호출")
         val request = DownloadManager.Request(Uri.parse(url)).apply {
             setTitle("오프라인 지도 다운로드")
             setDescription("지도 파일을 다운로드 중입니다.")
@@ -41,15 +42,17 @@ class MapDownloader(private val context: Context) {
                 if (cursor.moveToFirst()) {
                     val statusIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
                     val status = cursor.getInt(statusIndex)
+                    val message: String
                     if (status == DownloadManager.STATUS_SUCCESSFUL || status == DownloadManager.STATUS_FAILED) {
-                        val message: String
-                        if(status == DownloadManager.STATUS_SUCCESSFUL) {
-                            message  = "서버에서 지도 파일을 다운로드를 완료했습니다."
+                        if (status == DownloadManager.STATUS_SUCCESSFUL) {
+                            message = "다운로드를 성공적으로 완료하였습니다."
+                            Log.i("DownLoadManager", "status_successful 된 경우에 출력되는 로그")
                             sharedPref.setBoolean("downloadedMap", true)
-                        }else{
-                            message  = "서버에서 지도 파일을 다운로드를 취소했습니다."
+                        } else {
+                            message = "다운로드가 완료되지 못했습니다."
                         }
                         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+                        unregisterReceiver()
                     }
                 }
                 cursor.close()
@@ -58,7 +61,23 @@ class MapDownloader(private val context: Context) {
     }
 
     fun registerReceiver() {
-        context.registerReceiver(downloadReceiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            context.registerReceiver(
+                downloadReceiver,
+                IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE),
+                null,
+                null,
+                Context.RECEIVER_EXPORTED
+            )
+        } else {
+            context.registerReceiver(
+                downloadReceiver,
+                IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE),
+                null,
+                null,
+                Context.RECEIVER_VISIBLE_TO_INSTANT_APPS,
+            )
+        }
     }
 
     fun unregisterReceiver() {
