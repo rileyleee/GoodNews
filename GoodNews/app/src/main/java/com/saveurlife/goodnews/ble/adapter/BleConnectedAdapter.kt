caf2489ad3c6c6ccb321bc6b134fb7c988da8874
seondy <seondy99@gmail.com>
2024-02-23 18:05:01 +0900

    feat: 지도보기 ui 추가 및 임시 코드 저장

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/ble/adapter/BleConnectedAdapter.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/ble/adapter/BleConnectedAdapter.kt
@@ -1,11 +1,13 @@
 package com.saveurlife.goodnews.ble.adapter
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.saveurlife.goodnews.R
 import com.saveurlife.goodnews.ble.BleMeshConnectedUser
 import com.saveurlife.goodnews.databinding.ItemAroundListBinding
+import com.saveurlife.goodnews.map.MiniMapDialogFragment
 import kotlin.math.atan2
 import kotlin.math.cos
 import kotlin.math.pow
@@ -34,6 +36,20 @@ class BleConnectedAdapter(private val userList: List<BleMeshConnectedUser>) : Re
 
         holder.binding.aroundChatting.setOnClickListener {
             onChattingButtonClickListener?.onChattingButtonClick(user)
+        }
+        
+        // 연결된 사람의 채팅하기 버튼을 눌렀을 때 미니맵 띄우기
+        holder.binding.requestMinimapButton.setOnClickListener {
+            // 임시 미니맵
+            val miniMapFragment = MiniMapDialogFragment()
+            val otherUserLocation = Bundle()
+            otherUserLocation.putDouble("latitude", 36.321655)
+            otherUserLocation.putDouble("longitude", 127.378953)
+
+            miniMapFragment.arguments = otherUserLocation
+
+            // 다이얼로그를 보여주는 코드 필요 (manager 추가)
+            // miniMapFragment.show(childFragmentManager, "MiniMapDialogFragment")
         }
     }
 
