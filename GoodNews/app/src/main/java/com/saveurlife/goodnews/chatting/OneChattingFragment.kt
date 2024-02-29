3bf91d642ae7c268e4421167193624027afbdbc0
Lee EunGyeong <eunclid0202@gmail.com>
2024-02-29 23:56:40 +0900

    fix: 채팅 리스트에 띄울 때 연결 여부 확인 하고 배경색 다르게 처리(현재 BLE 연결 시에만 작동하는 이슈 발생)

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/chatting/OneChattingFragment.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/chatting/OneChattingFragment.kt
@@ -10,40 +10,29 @@ import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.saveurlife.goodnews.ble.BleMeshConnectedUser
 import com.saveurlife.goodnews.common.SharedViewModel
 import com.saveurlife.goodnews.databinding.FragmentOneChattingBinding
 
 class OneChattingFragment : Fragment() {
     val sharedViewModel: SharedViewModel by activityViewModels()
     private val chatDataList = mutableListOf<OnechattingData>()
+    private lateinit var users: List<BleMeshConnectedUser>
+    private lateinit var adapter : OneChattingAdapter
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?
     ): View? {
+
         var binding = FragmentOneChattingBinding.inflate(inflater, container, false)
 
-        val adapter = OneChattingAdapter(chatDataList)
-        val recyclerView = binding.recyclerViewChatting
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.adapter = adapter
+        // 연결된 사용자 리스트 가져오기
+        getChattingListsWhenConnected(binding)
 
-        loadChatRooms(adapter)
-
-
-        adapter.listener = object : OneChattingAdapter.OnItemClickListener {
-            override fun onItemClick(chatData: OnechattingData) {
-                val intent = Intent(context, ChattingDetailActivity::class.java).apply {
-                        putExtra("chatRoomId", chatData.chatRoomId)
-                        putExtra("chatName", chatData.otherName)
-                        putExtra("chatOtherStatus", chatData.otherStatus)
-                        putExtra("page",2)
-                    }
-                startActivity(intent)
-            }
-        }
         return binding.root
     }
 
     private fun loadChatRooms(adapter: OneChattingAdapter) {
+
         sharedViewModel.bleService.value?.let { bleService ->
             bleService.allChatRoomIds.observe(viewLifecycleOwner, Observer { chatRoomIds ->
                 Log.i("check", chatRoomIds.toString())
@@ -88,5 +77,43 @@ class OneChattingFragment : Fragment() {
         chatDataList.add(chatData)
         adapter.notifyDataSetChanged()
     }
+
+    private fun getChattingListsWhenConnected(binding: FragmentOneChattingBinding) {
+
+        // BLE로 연결된 이용자가 없으면 작동하지 않음
+        sharedViewModel.bleMeshConnectedDevicesMapLiveData.observe(
+            viewLifecycleOwner,
+            Observer { connectedDevicesMap ->
+                Log.v("OneChattingFragment","옵저버 가넝~")
+                // 중첩된 맵에서 BleMeshConnectedUser 추출
+                users = connectedDevicesMap.flatMap { it.value.values }.toList()
+                Log.v("연결된 사용자 수: ", users.size.toString())
+                adapter = OneChattingAdapter(chatDataList, users)
+                setupRecyclerView(binding)
+                loadChatRooms(adapter)
+
+            })
+        // 그래서 여기서 어댑터가 초기화 되지 않기 때문에 터짐
+        // Log.v("없다 있다 어떻게 구분",adapter.toString())
+    }
+
+    private fun setupRecyclerView(binding: FragmentOneChattingBinding) {
+        val recyclerView = binding.recyclerViewChatting
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = adapter
+
+        adapter.listener = object : OneChattingAdapter.OnItemClickListener {
+            override fun onItemClick(chatData: OnechattingData) {
+                val intent = Intent(context, ChattingDetailActivity::class.java).apply {
+                    putExtra("chatRoomId", chatData.chatRoomId)
+                    putExtra("chatName", chatData.otherName)
+                    putExtra("chatOtherStatus", chatData.otherStatus)
+                    putExtra("page",2)
+                }
+                startActivity(intent)
+            }
+        }
+    }
+
 
 }
