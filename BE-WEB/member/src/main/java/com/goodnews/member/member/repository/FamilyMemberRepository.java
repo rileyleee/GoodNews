b64914efe853841c34bf2d0f089a1dcdcbe08752
김예진 <kky156@naver.com>
2024-03-07 02:55:11 +0900

    refactor : family 관련 로직 전체 변경

--- BE-WEB/member/src/main/java/com/goodnews/member/member/repository/FamilyMemberRepository.java
+++ BE-WEB/member/src/main/java/com/goodnews/member/member/repository/FamilyMemberRepository.java
@@ -1,16 +1,20 @@
 package com.goodnews.member.member.repository;
 
 import com.goodnews.member.member.domain.FamilyMember;
+import com.goodnews.member.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface FamilyMemberRepository extends JpaRepository<FamilyMember,Integer> {
+public interface FamilyMemberRepository extends JpaRepository<FamilyMember, Integer> {
 
-    Optional<FamilyMember> findByMemberIdAndFamilyFamilyId(String memberId, String familyId);
+    Optional<FamilyMember> findByMemberIdAndFamilyFamilyId(String memberId, int familyId);
     Optional<FamilyMember> findByMemberId(String memberId);
 
+    Optional<FamilyMember> findByMemberIdAndApproveIsTrue(String memberId);
 
     List<FamilyMember> findByMemberIdAndApproveIsFalse(String memberId);
+
+    List<FamilyMember> findByFamilyFamilyId(int familyId);
 }
