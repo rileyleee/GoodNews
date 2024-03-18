a85d5daa00ac8a610554f081097f99bbce3b6dcd
Lee EunGyeong <eunclid0202@gmail.com>
2024-03-18 23:49:18 +0900

    chore: 위치 공개하기 기본값을 true로 설정

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/GoodNewsApplication.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/GoodNewsApplication.kt
@@ -52,6 +52,9 @@ class GoodNewsApplication : Application(), Application.ActivityLifecycleCallback
         // 앱 실행 시마다 지도 프래그먼트 로드 가능 여부는 false로 초기화
         preferences.setBoolean("canLoadMapFragment", false)
 
+        // 내 위치 공개하기 기능 -> 기본 값 true
+        preferences.setBoolean("myLocation", true)
+
         super.onCreate()
 
         // Google Places API 초기화
